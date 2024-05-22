3346bbc1fd5f67f32f7666286ffaaec4bff305ae
John Mwanyika <mwanyikajohn@outlook.com>
2024-05-22 17:58:28 +0300

    Refactor: change icon slider layout to flex box

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -203,7 +203,38 @@ $(function () {
         infinite: true,
         slidesToShow: 8,
         slidesToScroll: 1,
-        // variableWidth: true
+        prevArrow: $('.categoryPrev'),
+        nextArrow: $('.categoryNext'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    accessibility: false
+
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 
     $('.imageSlider').slick({
